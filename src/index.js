a3647660d7bfb72a45b3abce2088f2181d058803
vovBorya <vovbor02@gmail.com>
2020-05-07 16:37:06 +0300

    Added modal box

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+//modal - для манипуляции над модальным окном
+const modal = document.getElementById("modal");
+
+const colorArray = ["rgb(34, 49, 63)", "rgb(44, 62, 80)", "rgb(58, 83, 155)"];
+
 function togglePicture() {
   const label = document.getElementById("label");
   const image = document.getElementById("os-logo");
@@ -8,4 +13,31 @@ function togglePicture() {
     label.innerHTML = "Linux";
     image.src = "resource/linux-logo.png"
   }
+}
+
+function showDialogBox() {
+    modal.style.display = "block";
+}
+
+function closeDialogBox() {
+  modal.style.display = "none";
+}
+
+function submitDialogBox() {
+  const inputText = document.getElementById("input").value;
+  document.getElementById("welcome-text").innerText = `Hello, ${inputText}! How are you?`;
+  closeDialogBox();
+}
+
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
+
+function changeBackground() {
+  const background = document.getElementById("container");
+  console.log(background.style.background);
+  background.style.background = "rgb(44, 62, 80)";
+  console.log(background.style.background);
 }
